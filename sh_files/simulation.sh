1a49f070b16e1d87424fc0554056244337be584f
SarthakNarayan <sarthak.narayang@gmail.com>
2019-10-08 22:59:46 +0530

    Documentation with name change commit

--- sh_files/simulation.sh
+++ sh_files/simulation.sh
@@ -4,10 +4,10 @@
 source ~/.bashrc
 
 printf "Launching mavros , px4 , gazebo , Running QGround Control\n"
-xterm /home/tahseer/catkin_ws/src/openquad/sh_files/QGroundControl.sh &
-xterm /home/tahseer/catkin_ws/src/openquad/sh_files/mavros.sh &
-xterm /home/tahseer/catkin_ws/src/openquad/sh_files/px4.sh &
-xterm /home/tahseer/catkin_ws/src/openquad/sh_files/gazebo.sh 
+xterm ~/catkin_ws/src/openquad/sh_files/QGroundControl.sh &
+xterm ~/catkin_ws/src/openquad/sh_files/mavros.sh &
+xterm ~/catkin_ws/src/openquad/sh_files/px4.sh &
+xterm ~/catkin_ws/src/openquad/sh_files/gazebo.sh 
 
 printf "\nKilling gazebo client and server\n"
-/home/tahseer/catkin_ws/src/openquad/sh_files/kill.sh &
+~/catkin_ws/src/openquad/sh_files/kill.sh &
